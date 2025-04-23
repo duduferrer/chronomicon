b11b44d17f01d3167e0dec4c4d65c9c5ae2c654b
Eduardo Ferrer <duduferrer7@gmail.com>
2025-04-23 00:17:06 -0300

    feat(frontend): add login card to login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,12 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Login from './pages/Login'
-
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Login/>}/>
-      </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/components/Input.jsx
@@ -0,0 +1,18 @@
+const Input = ({ type, id, placeholder, htmlFor }) => {
+  return (
+    <label htmlFor={htmlFor} class="relative">
+      <input
+        type={type}
+        id={id}
+        placeholder=""
+        class="peer mt-0.5 w-full rounded border-gray-500 shadow-sm sm:text-sm h-11 bg-gray-100 px-5"
+      />
+
+      <span class="absolute left-3 top-1/2 -translate-y-1/2 bg-gray-100 px-1 text-sm font-medium text-gray-700 transition-all duration-200 peer-placeholder-shown:top-1/3 peer-placeholder-shown:translate-y-0 peer-focus:top-0 peer-focus:-translate-y-full">
+        {placeholder}
+      </span>
+    </label>
+  );
+};
+
+export default Input;
